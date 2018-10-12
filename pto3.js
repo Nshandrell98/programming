511a4f5b814a62b3d010f8ff614401da9ee3fa4c
nekiedra.singleton <nekiedrasingleton@gmail.com>
2018-10-12 12:17:31 -0500

    finished pto3

--- pto3.js
+++ pto3.js
@@ -1,7 +1,8 @@
 function pto(){
     event.preventDefault();
-
+    var output = document.getElementById("output");
     var yrsWrkd = parseInt(document.getElementById("yrsWrkd").value);
+    var hdw = Number(document.querySelector('input[name=HDW]:checked').value);
     var hrsPerCk;
     if (yrsWrkd < 5 ){
         hrsPerCk = 5;
@@ -12,9 +13,21 @@ function pto(){
     if (yrsWrkd >= 10){
         hrsPerCk = 7;
     }
-
+    var yearlyPTO = hrsPerCk * 26;
     var wksWrkd = parseInt(document.getElementById("wksWrkd").value);
     var totHrsToDate = (wksWrkd / 2) * hrsPerCk;
 
     var ptoTkn = parseInt(document.getElementById("ptoTkn").value);
+    var ptoLeft = yearlyPTO - ptoTkn;
+
+    if (hdw === 1){
+        ptoLeft = ptoLeft;
+    }
+    if (hdw === 2 ){
+        ptoLeft = ptoLeft / 8;
+    }
+    if (hdw === 3){
+        ptoLeft = ptoLeft / 40;
+    }
+    output.textContent = ptoLeft;
 }
